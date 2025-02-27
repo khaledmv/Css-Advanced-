0721b9821931be606ad473c550ed0341169e2a5a
khaledmv <khaledhossionmv@gmail.com>
2025-02-27 14:45:27 +0600

    Initialized mouse move event with gsap

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 
+let body = document.querySelector('body');
+let cursor = document.querySelector('.cursor');
 
 
-let text = document.querySelector('.text');
-
-document.addEventListener("mousemove", function(e){
-    text.style.left = e.pageX + 'px';
-    text.style.top = e.pageY + 'px';
-    text.style.transform = `rotate(${e.pageX / 2}deg)`;
-});
-
-
-// Split text
-
-text.innerHTML = text.textContent.replace(/\S/g,  '<span>$&</span>');
-
-let element = document.querySelectorAll('span');
-
-for(let i = 0; i < element.length; i++){
-    element[i].style.transform = "rotate("+i*18+"deg)";
+body.onclick = function(){
+    body.classList.toggle('dark');
 }
+
+
+for(let i = 0; i < 200; i++){
+    let cercle = document.createElement('div');
+    cercle.classList.add('cercle')
+    cursor.appendChild(cercle);
+}
+
+
+// integrate gsap
+
+document.body.addEventListener("mousemove", function(e){
+    gsap.to(".cercle",  {
+        x: e.clientX,
+        y: e.clientY,
+        stagger: -0.0025,
+        scale: (i, target) => {
+            return 1 + (i * (2 /200));
+        }
+    })
+})
